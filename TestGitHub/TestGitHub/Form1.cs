01a4e05b38b59920b2d66477f1dc1929d09892de
Vlad <vladasekretov@gmail.com>
2021-06-30 10:56:32 +0300

    simple clock

---
+++ TestGitHub/TestGitHub/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGitHub
+{
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.timer = new System.Windows.Forms.Timer();
+            this.timer.Tick += Timer_Tick;
+            this.timer.Start();
+
+
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            string clock = $"{DateTime.Now.Hour} : {DateTime.Now.Minute} : {DateTime.Now.Second}";
+            this.label.Text = clock;
+        }
+
+
+
+
+
+
+    }
+}
