b45ff35cf2de7faca8b8e5de97bef37ecee3888e
Vlad <vladasekretov@gmail.com>
2021-06-30 11:29:05 +0300

    added new form and the color is given to the first form

--- TestGitHub/TestGitHub/Form1.Designer.cs
+++ TestGitHub/TestGitHub/Form1.Designer.cs
@@ -42,23 +42,15 @@ namespace TestGitHub
             this.FormBorderStyle = FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.BackColor = Color.Purple;
             this.ResumeLayout(false);
             this.PerformLayout();
 
 
 
-            ///                 label
-            this.label = new Label();
-            this.label.BackColor = Color.White;
-            this.label.Size = new System.Drawing.Size(75, 35);
-            this.label.Location = new System.Drawing.Point(this.Width / 2 - (this.label.Width / 2), this.Height / 2 - (this.label.Height / 2));
-            this.label.Font = new System.Drawing.Font("Times New Roman", 20);
-            ///                 label               END
-
-
-
             ///                 panel
             this.panel = new Panel();
+            this.panel.BackColor = Color.Blue;
             this.panel.BorderStyle = BorderStyle.FixedSingle;
             this.panel.BackColor = Color.White;
             this.panel.Size = new System.Drawing.Size(400, 200);
@@ -66,13 +58,45 @@ namespace TestGitHub
             ///                 panel               END
 
 
-            this.Controls.Add(label);
+
+            ///                 label
+            this.label = new Label();
+            this.label.ForeColor = Color.Red;
+            this.label.BackColor = Color.Black;
+            this.label.Size = new System.Drawing.Size(75, 35);
+            this.label.Location = new System.Drawing.Point(this.panel.Width / 2 - (this.label.Width / 2), this.panel.Height / 2 - (this.label.Height / 2));
+            this.label.Font = new System.Drawing.Font("Times New Roman", 20);
+            this.panel.Controls.Add(label);
+            ///                 label               END
+
+
+
+            ///                 button
+            this.button = new Button();
+            this.button.BackColor = Color.Blue;
+            this.button.ForeColor = Color.Red;
+            this.button.Text = "Settings";
+            this.button.Size = new Size(60, 60);
+            this.button.Location = new Point(this.Width - 60, 0);
+            this.button.Click += Button_Click;
+            ///                 button              END
+
+
             this.Controls.Add(panel);
+            this.Controls.Add(button);
+        }
+
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            settings.ShowDialog();
         }
 
         #endregion
         private Label label;
         private Panel panel;
+        private Button button;
+        Settings settings = new Settings();
     }
 }
 
